9eff3f279a863d1a1d013caccdeea282bbdb2fa7
igor pirasoli <igorpirasoli@outlook.com>
2024-08-04 22:22:21 -0300

    excessoes

---
+++ tratamento de excessoes/Try_catch/Try_catch/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3 };
+            try
+            {
+                Console.WriteLine(array[3]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"Exceção capturada: {e.Message}");
+            }
+        }
+    }
+}
