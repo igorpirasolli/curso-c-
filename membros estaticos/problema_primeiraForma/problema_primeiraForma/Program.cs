13b83a54fb16cb21673fe22955f402a190fd4b64
igor pirasoli <igorpirasoli@outlook.com>
2024-08-09 19:36:51 -0300

    membros estaticos

---
+++ membros estaticos/problema_primeiraForma/problema_primeiraForma/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace problema_primeiraForma
+{
+    class Program
+    {
+        static double pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entre com o valor do raio:");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Circunferencia(raio);
+            Console.WriteLine($"circunferencia : {circ}");
+
+            double volume = Volume(raio);
+            Console.WriteLine($"Volume: {volume:n}");
+
+
+
+        }
+
+        static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * pi * Math.Pow(raio, 3);
+        }
+
+        static double Circunferencia(double raio)
+        {
+            return 2 * pi * raio;
+        }
+    }
+}
---
+++ membros estaticos/segunda_forma/segunda_forma/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace segunda_forma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circunferencia = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferencia = {circunferencia:n}");
+            Console.WriteLine($"Volume = {volume:n}");
+            Console.WriteLine($"pi = {Calculadora.pi}");
+
+
+        }
+    }
+}
